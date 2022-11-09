1f7501d3c3abeed30658177347e01578f21947f9
kouta1206 <ko.bump1206@icloud.com>
2022-11-09 09:44:00 +0900

    2章終了

---
+++ app/models/ranks_updater.rb
@@ -0,0 +1,15 @@
+class RanksUpdater
+  def update_all
+  # 現在のランキング情報をリセット
+    Rank.delete_all
+
+    create_ranks
+  end
+
+  private
+
+  def create_ranks
+    RankOrderMaker.new.each_ranked_user do |user, rank|
+      Rank.create(user_id: user.id, rank: rank, score: user.total_score)
+    end
+  end
